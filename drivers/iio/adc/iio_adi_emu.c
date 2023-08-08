ff5adb610afe369be3b8e718931ed1977543713e
chegbeli <ciprian.hegbeli@analog.com>
2023-08-08 16:01:10 +0300

    iio: iio_adi_emu: Add channels with read raw
    Added two channels which can be read and have some random values.
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -10,8 +10,41 @@
 
 #include <linux/iio/iio.h>
 
-static const struct iio_info adi_emu_info = {
+static int adi_emu_read_raw(struct iio_dev *indio_dev,
+			    struct iio_chan_spec const *chan,
+			    int *val,
+			    int *val2,
+			    long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			*val = 10;
+		else
+			*val = 22;
+		return IIO_VAL_INT;
+	default:
+		return -EINVAL;
+	}
+}
 
+static const struct iio_info adi_emu_info = {
+	.read_raw = &adi_emu_read_raw,
+};
+
+static const struct iio_chan_spec adi_emu_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 };
 
 static int adi_emu_probe(struct spi_device *spi)
@@ -24,6 +57,8 @@ static int adi_emu_probe(struct spi_device *spi)
 	
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
+	indio_dev->channels = adi_emu_channel;
+	indio_dev->num_channels = ARRAY_SIZE(adi_emu_channel);
 
 	return devm_iio_device_register(&spi->dev,indio_dev);
 }
@@ -36,6 +71,6 @@ static struct spi_driver adi_emu_driver = {
 };
 module_spi_driver(adi_emu_driver);
 
-MODULE_AUTHOR("Ciprian Hegbeli <ciprian.hegbeli@analog.com");
+MODULE_AUTHOR("Ciprian Hegbeli <ciprian.hegbeli@analog.com>");
 MODULE_DESCRIPTION("Analog Devices ADC Emulator Driver");
 MODULE_LICENSE("GPL v2");
