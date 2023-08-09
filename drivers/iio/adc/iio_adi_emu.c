f7f7545795d37af204423b91d27b08f6aabcfc1f
chegbeli <ciprian.hegbeli@analog.com>
2023-08-09 10:44:58 +0300

    iio: adc: iio_adi_emu: Add priv struct and write
    Added the write raw function and the private structure for an enable
    attribute. This attribute can be read and written.
    
    Signed-off-by: chegbeli <ciprian.hegbeli@analog.com>

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -10,12 +10,18 @@
 
 #include <linux/iio/iio.h>
 
+struct adi_emu_state {
+	bool en;
+};
+
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val,
 			    int *val2,
 			    long mask)
 {
+	struct adi_emu_state *st = iio_priv(indio_dev);
+
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
 		if (chan->channel)
@@ -23,6 +29,26 @@ static int adi_emu_read_raw(struct iio_dev *indio_dev,
 		else
 			*val = 22;
 		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->en;
+		return IIO_VAL_INT;	
+	default:
+		return -EINVAL;
+	}
+}
+
+static int adi_emu_write_raw(struct iio_dev *indio_dev,
+			     struct iio_chan_spec const *chan,
+			     int val,
+			     int val2,
+			     long mask)
+{
+	struct adi_emu_state *st = iio_priv(indio_dev);
+
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		st->en = val;
+		return 0;
 	default:
 		return -EINVAL;
 	}
@@ -30,6 +56,7 @@ static int adi_emu_read_raw(struct iio_dev *indio_dev,
 
 static const struct iio_info adi_emu_info = {
 	.read_raw = &adi_emu_read_raw,
+	.write_raw = &adi_emu_write_raw,
 };
 
 static const struct iio_chan_spec adi_emu_channel[] = {
@@ -38,23 +65,29 @@ static const struct iio_chan_spec adi_emu_channel[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 };
 
 static int adi_emu_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
+	struct adi_emu_state *st;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 	if (!indio_dev)
 		return -ENOMEM;
-	
+
+	st = iio_priv(indio_dev);
+	st->en = 0;
+
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
 	indio_dev->channels = adi_emu_channel;
