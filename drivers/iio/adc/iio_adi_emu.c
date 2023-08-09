4f33acbada94a97caa5988710d16923b44383fe6
chegbeli <ciprian.hegbeli@analog.com>
2023-08-09 14:24:09 +0300

    iio: adc: iio_adi_emu: Add debug var in priv for chans

--- drivers/iio/adc/iio_adi_emu.c
+++ drivers/iio/adc/iio_adi_emu.c
@@ -12,6 +12,8 @@
 
 struct adi_emu_state {
 	bool en;
+	u16 tmp_chan0;
+	u16 tmp_chan1;
 };
 
 static int adi_emu_read_raw(struct iio_dev *indio_dev,
@@ -25,9 +27,9 @@ static int adi_emu_read_raw(struct iio_dev *indio_dev,
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
 		if (chan->channel)
-			*val = 10;
+			*val = st->tmp_chan1;
 		else
-			*val = 22;
+			*val = st->tmp_chan0;
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_ENABLE:
 		*val = st->en;
@@ -46,6 +48,12 @@ static int adi_emu_write_raw(struct iio_dev *indio_dev,
 	struct adi_emu_state *st = iio_priv(indio_dev);
 
 	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			st->tmp_chan1 = val;
+		else
+			st->tmp_chan0 = val;
+		return 0;
 	case IIO_CHAN_INFO_ENABLE:
 		st->en = val;
 		return 0;
@@ -87,6 +95,8 @@ static int adi_emu_probe(struct spi_device *spi)
 
 	st = iio_priv(indio_dev);
 	st->en = 0;
+	st->tmp_chan0 = 0;
+	st->tmp_chan1 = 0;
 
 	indio_dev->name = "iio-adi-emu";
 	indio_dev->info = &adi_emu_info;
