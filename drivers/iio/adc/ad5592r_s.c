ef65f5348ac543877ec45536842944bbafcad0c7
Radu Sabau <radu.sabau@analog.com>
2023-08-09 09:51:34 +0300

    iio : adc : ad5592r : Added support for read and chan
    Added read_raw and chan structures statically.

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -11,8 +11,93 @@
 #include <linux/iio/iio.h>
 
 
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+			      struct iio_chan_spec const *chan,
+			      int *val,
+			      int *val2,
+			      long mask)
+{
+	switch(mask) {
+	case IIO_CHAN_INFO_RAW:
+		switch(chan->channel){
+			case 0:
+				*val = 0;
+				break;
+			case 1:
+		 		*val = 1;
+				break;
+			case 2:
+				*val = 2;
+				break;
+			case 3:
+				*val = 3;
+				break;
+			case 4:
+				*val = 4;
+				break;
+			case 5:
+				*val = 5;
+				break;
+
+			default :
+
+				*val = 100;
+				break;
+		}
+
+		return IIO_VAL_INT;
+	
+	default :
+		return -EINVAL;
+	
+	}
+}
+
+static const struct iio_info adi_emu_info = {
+	.read_raw = &ad5592r_s_read_raw,
+};
+
 static const struct iio_info ad5592r_s_info = {
 
+};
+
+static const struct iio_chan_spec ad5592r_s_channel[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 2,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 3,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 4,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.channel = 5,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	},
 };
 
 static int ad5592r_probe(struct spi_device *spi)
@@ -25,6 +110,8 @@ static int ad5592r_probe(struct spi_device *spi)
 	
 	indio_dev->name = "ad5592r_s";
 	indio_dev->info = &ad5592r_s_info;
+	indio_dev->channels = ad5592r_s_channel;
+	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channel);
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
